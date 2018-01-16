95185c7123ef4d83569e6a1f3ef507243374b2cd
charliecaiftaa <charlie.cai@g.feitianacademy.org>
2018-01-16 12:09:20 -0500

    Commit Test

--- Charlie_Test.js
+++ Charlie_Test.js
@@ -1,28 +1,32 @@
 requirejs(['../src/WorldWind',
-        './TestLayerManager'],
+        './../LayerManager'],
     function (ww,
               LayerManager) {
         "use strict";
 
-        // Tell World Wind to log only warnings.
         WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
 
-        // Create the World Window.
         var wwd = new WorldWind.WorldWindow("canvasOne");
 
-        /**
-         * Added imagery layers.
-         */
         var layers = [
             {layer: new WorldWind.BMNGLayer(), enabled: true},
             {layer: new WorldWind.BMNGLandsatLayer(), enabled: false},
+            {layer: new WorldWind.BingAerialLayer(null), enabled: false},
             {layer: new WorldWind.BingAerialWithLabelsLayer(null), enabled: true},
+            {layer: new WorldWind.BingRoadsLayer(null), enabled: false},
             {layer: new WorldWind.CompassLayer(), enabled: true},
             {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
             {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
+
         ];
 
         for (var l = 0; l < layers.length; l++) {
             layers[l].layer.enabled = layers[l].enabled;
             wwd.addLayer(layers[l].layer);
-        }}
+        }
+
+        // Create a layer manager for controlling layer visibility.
+        var layerManger = new LayerManager(wwd);
+
+        wwd.redraw();
+    });
